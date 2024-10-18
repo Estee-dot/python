c045a5bb7e157b8f677411f5eb8e3a92a8ba0002
Esther Ikechukwu <estherik18@gmail.com>
2024-10-18 20:57:52 +0100

    assignment

---
+++ assignment/radius.py
@@ -0,0 +1,10 @@
+radius = int(input("Enter the radius of the circle: "))
+pi = 3.142
+diameter = 2 * radius
+print(diameter)
+
+area = pi * (radius * radius)
+print(area)
+
+circumference = 2 * pi * radius
+print(circumference)
