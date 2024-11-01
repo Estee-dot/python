5fac118910229bf28e1320e4f9d0dbac1e164948
Esther Ikechukwu <estherik18@gmail.com>
2024-11-01 07:49:39 +0100

    contination

---
+++ assignment/increasing_decreasing.py
@@ -0,0 +1,15 @@
+firstNumber = int(input("Enter the first number: "))
+
+secondNumber = int(input("Enter the second number: "))
+
+thirdNumber = int(input("Enter the third number: "))
+
+if firstNumber > secondNumber && secondNumber > thirdNumber:
+    print("they are in decreasing order");
+		
+else if firstNumber < secondNumber && secondNumber < thirdNumber:
+   print("they are in increasing order")
+
+else{
+    print("invalid input")
+		
---
+++ assignment/libary.py
@@ -0,0 +1,13 @@
+numberOfDays = int(input("Enter the number of days a member is late to return the book: "))
+if numberOfDays == 5: 
+print("fine is 50 paise")
+
+else if numberOfDays == 6 && numberOfDays < 10:
+print("fine is 1 Rupee")
+
+else if numberOfDays == 10:
+print("fine is 5 Rupee")
+
+else if numberOfDays  >= 30:
+print("membership is cancelled")
+	
