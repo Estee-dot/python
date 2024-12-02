d2de9aa7b0caec8febe9425c29d0586a7de670a4
Esther Ikechukwu <estherik18@gmail.com>
2024-12-02 16:59:38 +0100

    phasegate

--- phaseGateOne/functionsecurepassword.py
+++ phaseGateOne/functionsecurepassword.py
@@ -1,11 +1,15 @@
-pseudo code: passwords
-- Create a password generator function
-- password must have lowercase letters
-- password must have uppercase letters
-- password must contain numbers
-- password must contain symbols
-- len(password) == 16 characters
+#pseudo code: passwords
+#- Create a password generator function
+#- password must have lowercase letters
+#- password must have uppercase letters
+#- password must contain numbers
+#- password must contain symbols
+#- len(password) == 16 characters
 
-def get_secure_password():
 
+def get_secure_password(password : str):
+	symbols =[!, @ , #, $, %, ^, &, *, ~, +, -, _, ", ?, |]
+	number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+	letters = letter.upper() 
+	
 	
--- phaseGateOne/test_functionpasword.py
+++ phaseGateOne/test_functionpasword.py
@@ -3,4 +3,7 @@ import functionsecurepassword
 
 class TestSecurePasswordFunction(unittest, TestCase):
 	def test_that_get_secure_password_exist(self):
-		functionsecurepassword.get_secure_password(-)
+		functionsecurepassword.get_secure_password(password)
+
+	
+		
